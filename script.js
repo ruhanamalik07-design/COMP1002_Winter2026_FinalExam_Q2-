6c4cfdde3d7cf79940ed2a38a57896f29525ee10
ruhana malik <ruhanamalik07@gmail.com>
2026-04-16 10:35:39 -0400

    Added JavaScript submit event with personalized message

---
+++ script.js
@@ -0,0 +1,18 @@
+//Select the form
+const form = document.getElementById("registrationForm");
+
+//add submit event listener
+form.addEventListener("submit" , function(event){
+
+    //Prevent default form submission
+    event.preventDefault();
+
+    //Get user's name
+    const name = document.getElementById("name").ariaValueMax;
+
+    //Display personalised message
+    const message = document.getElementById("message");
+    message.textContent = `Thank you, ${name}! You have registered successfully. `;
+    
+
+});
